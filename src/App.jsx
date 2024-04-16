3e653567bdc3c11d0a55e3c0480abd2afac0e5b0
David Henrique <dh210605@gmail.com>
2024-04-16 19:18:39 -0300

    feat: atualização de dados

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,36 @@
-import react from 'react';
-import './App.css'
-import Aulass from './pages/Aulas/Aulass';
-
+import React, { useState } from 'react';
 
 function App() {
+  const [nome, setNome] = useState('');
+  const [idade, setIdade] = useState('');
 
+  const handleNomeChange = (event) => {
+    setNome(event.target.value);
+  };
+
+  const handleIdadeChange = (event) => {
+    setIdade(event.target.value);
+  };
+
+  const handleClick = () => {
+    alert(`Nome: ${nome} , Idade: ${idade}`);
+  };
 
   return (
-  
-        <Aulass/>
-
-  
-  )
+    <div>
+      <label>
+        Nome:
+        <input type="text" value={nome} onChange={handleNomeChange} />
+      </label>
+      <br />
+      <label>
+        Idade:
+        <input type="text" value={idade} onChange={handleIdadeChange} />
+      </label>
+      <br />
+      <button onClick={handleClick}>Exibir Informações</button>
+    </div>
+  );
 }
 
 export default App;
